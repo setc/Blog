ecb400d4b286acf3aed3771e1b1bd0e8a134b449
Sebastian Torrente <sebastian.torrente@gmail.com>
2013-04-03 21:54:11 +0100

    Damn you ascii error

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 
-AUTHOR = 'Sebastian Torrente'
-SITENAME = 'Infrafrequency'
+AUTHOR = u'Sebastian Torrente'
+SITENAME = u'Infrafrequency'
 SITEURL = r'http://infrafrequency.herokuapp.com'
-
 TIMEZONE = 'Europe/London'
+
+THEME = 'brownstone'
+
+# Other things
+GITHUB_URL = r'https://github.com/SebastianTorrente/'
+#DISQUS_SITENAME = ''
+PDF_GENERATOR = False
+REVERSE_CATEGORY_ORDER = True
+LOCALE = 'C'
+DEFAULT_PAGINATION = 5
+
+FEED_ALL_RSS = 'feeds/all.rss.xml'
 
 DEFAULT_LANG = u'en'
 
-# Theme
-THEME = "brownstone"
-
 # Categories
 DEFAULT_CATEGORY = "Blog"
+USE_FOLDER_AS_CATEGORY = True
+
 
 # Blogroll
 LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
@@ -24,5 +34,3 @@ LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
 SOCIAL =  (('GitHub', r'https://github.com/SebastianTorrente'),
           ('LinkedIn', r'http://uk.linkedin.com/in/sebastiantorrentecarrillo/'),
 	  ('Twitter', r'http://twitter.com/infrafrequency'),)
-
-DEFAULT_PAGINATION = 5
